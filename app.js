e5e716331bf6b81b7d2d334e4c6559a94a015820
TechCommunity-debug <techcommunity611@gmail.com>
2025-04-13 20:23:30 +0530

    Sending Responses.

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ const http = require("http");
 
 // WAY 3
 const server = http.createServer((req, resp) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+
+  resp.setHeader('Content-Type', 'text/html');
+  
+  resp.write('<html>');
+  resp.write('<head><title>Hello World</title></header>');
+  resp.write('<body><h1>Hello World from my node server.</h1></body>');
+  resp.write('</html>');
 });
 
 server.listen(3000);
