2c1409677409e69c45990837727d8ebd69a5d484
TechCommunity-debug <techcommunity611@gmail.com>
2025-04-14 22:59:21 +0530

    Raw NodeJs: Using the Node Modules System (Routing)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -20,42 +20,8 @@ const fs = require("fs");
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // WAY 3
-const server = http.createServer((req, resp) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/') {
-    resp.write('<html>');
-    resp.write('<head><title>Enter Message</title></header>');
-    resp.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
-    resp.write('</html>');
-    return resp.end();
-  }
-
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on('end',() => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-    });
-    resp.statusCode = 302;
-    resp.setHeader('Location', '/');
-    return resp.end();
-  }
-
-  resp.setHeader('Content-Type', 'text/html');
-  
-  resp.write('<html>');
-  resp.write('<head><title>Hello World</title></header>');
-  resp.write('<body><h1>Hello World from my node server.</h1></body>');
-  resp.write('</html>');
-  resp.end();
-});
+//const server = http.createServer(routes);
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
