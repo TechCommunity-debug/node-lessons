a4eb9033590957ec10cc99d2063d0eef4bb73007
TechCommunity-debug <techcommunity611@gmail.com>
2025-04-20 02:11:16 +0530

    Change Project.

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+const path = require('path');
 
-const rootDir = require("./util/path");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// only routes starting with /admin (like /admin/add-product) will go into adminRoutes.
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
