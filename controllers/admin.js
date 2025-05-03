2dd237b4897f56c9d88ba4115d49e19c034e2358
TechCommunity-debug <techcommunity611@gmail.com>
2025-05-03 14:12:58 +0530

    MySQL: Sequelize: Updating Products.

--- controllers/admin.js
+++ controllers/admin.js
@@ -31,7 +31,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
+  Product.findByPk(prodId).then((product) => {
     if (!product) {
       return res.redirect("/");
     }
@@ -41,8 +41,8 @@ exports.getEditProduct = (req, res, next) => {
       editing: editMode,
       product: product,
     });
-  });
-};
+  }).catch((err) => console.log("err :>> ", err))
+  };
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
@@ -50,25 +50,28 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect("/admin/products");
+  Product.findByPk(prodId).then((product) => {
+   product.title = updatedTitle;
+   product.price = updatedPrice;
+   product.imageUrl = updatedImageUrl;
+   product.updatedDesc = updatedDesc;
+   return product.save();
+  })
+  .then(result => {
+    console.log("UPDATED PRODUCT!!");
+    res.redirect("/admin/products");
+  })
+  .catch((err) => console.log("err :>> ", err))
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
+  Product.findAll().then(products => {
     res.render("admin/products", {
       prods: products,
       pageTitle: "Admin Products",
       path: "/admin/products",
     });
-  });
+  }).catch((err) => console.log("err :>> ", err))
 };
 
 exports.postDeleteProduct = (req, res, next) => {
